58369efe4a827c50e033e4487f28ccd0139cecf8
CoolCyberBrain <8357970+CoolCyberBrain@users.noreply.github.com>
2019-01-26 20:38:02 -0800

    Update SharedHandler.java

--- android/src/main/java/in/sriraman/sharedpreferences/SharedHandler.java
+++ android/src/main/java/in/sriraman/sharedpreferences/SharedHandler.java
@@ -1,78 +1,78 @@
 package in.sriraman.sharedpreferences;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import java.util.Map;
 
-public class SharedDataProvider {
+public class SharedHandler {
 
-    private static final String TAG = "SharedDataProvider";
+    private SharedPreferences mSharedPreferences;
 
-     public static String[][] getMultiSharedValues(String[] keys) {
-        SharedHandler sharedHandler = SharedHandler.getInstance();
-        String[][] results = new String[keys.length][2];
-        for (int i = 0; i < keys.length; i++) {
-            results[i][0] = keys[i];
-            results[i][1] = String.valueOf(sharedHandler.getString(keys[i]));
+    private static SharedHandler sSharedHandler;
+
+    public SharedHandler(Context context) {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static SharedHandler getInstance() {
+        return sSharedHandler;
+    }
+
+    public static void init(Context context) {
+	if (sSharedHandler==null) {
+            sSharedHandler = new SharedHandler(context);
+	}
+    }
+
+    public void putExtra(String key, Object value) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        if (value instanceof String) {
+            editor.putString(key, (String) value).apply();
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) value).apply();
+        } else if (value instanceof Integer) {
+            editor.putInt(key, (Integer) value).apply();
+        } else if (value instanceof Long) {
+            editor.putLong(key, (Long) value).apply();
+        } else if (value instanceof Float) {
+            editor.putFloat(key, (Float) value).apply();
         }
-        return results;
     }
 
-		public static String[][] getAllSharedValues() {
-        Map<String, ?> keyValues = SharedHandler.getInstance().getAllSharedData();
-        List<String> keys = new ArrayList<>(keyValues.keySet());
-        String[][] results = new String[keys.size()][2];
-        for (int i = 0; i < keys.size(); i++) {
-            results[i][0] = keys.get(i);
-            results[i][1] = String.valueOf(keyValues.get(keys.get(i)));
-        }
-        return results;
-    }
-		
-		/*public static String[] getMultiSharedValues(String[] keys) {
-        SharedHandler sharedHandler = SharedHandler.getInstance();
-        String[] results = new String[keys.length];
-        for (int i = 0; i < keys.length; i++) {
-            results[i] = sharedHandler.getString(keys[i]);
-        }
-        return results;
+    public String getString(String key) {
+        return mSharedPreferences.getString(key, null);
     }
 
-    public static String[][] getMultiSharedValues(String[] keys) {
-        SharedHandler sharedHandler = SharedHandler.getInstance();
-        String[][] results = new String[keys.length][2];
-        for (int i = 0; i < keys.length; i++) {
-            results[i][0] = keys[i];
-            results[i][1] = String.valueOf(sharedHandler.getString(keys[i]));
-        }
-        return results;
-    }
-    */
-
-    public static String[] getAllKeys() {
-        Map<String, ?> keyValues = SharedHandler.getInstance().getAllSharedData();
-        List<String> keys = new ArrayList<>(keyValues.keySet());
-        String[] results = new String[keys.size()];
-        for (int i = 0; i < keys.size(); i++) {
-            results[i] = keys.get(i);
-        }
-        return results;
+    public Float getFloat(String key) {
+        return mSharedPreferences.getFloat(key, 0f);
     }
 
-    public static String getSharedValue(String key) {
-        return SharedHandler.getInstance().getString(key);
+    public Long getLong(String key) {
+        return mSharedPreferences.getLong(key, 0);
     }
 
-    public static void putSharedValue(String key, String value) {
-        SharedHandler.getInstance().putExtra(key, value);
+    public Boolean getBoolean(String key) {
+        return mSharedPreferences.getBoolean(key, false);
     }
 
-    public static void clear() {
-        SharedHandler.getInstance().clear();
+    public Integer getInt(String key) {
+        return mSharedPreferences.getInt(key, 0);
     }
 
-    public static void deleteSharedValue(String key) {
-        SharedHandler.getInstance().deleteKey(key);
+    public void clear() {
+        mSharedPreferences.edit().clear().apply();
+    }
+
+    public Map<String, ?> getAllSharedData(){
+        return mSharedPreferences.getAll();
+    }
+
+    public void deleteKey(String key) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.remove(key);
+        editor.apply();
     }
 
 }
